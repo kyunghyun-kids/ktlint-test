96c298155e193ad5c6efafbc21c071c3814de280
kyunghyun-kids <intern226065@lguplus.co.kr>
2022-04-24 19:56:25 +0900

    init

---
+++ src/main/kotlin/com/example/ktlinttest/KtlintTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ktlinttest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtlintTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtlintTestApplication>(*args)
+}
